dbd2b26718ecea06173dd2896b74e0e0915ffdd4
Petrando <bangkitwisata@gmail.com>
2021-08-10 18:39:13 +0800

    fix: css fixes

--- pages/admin/interests.tsx
+++ pages/admin/interests.tsx
@@ -13,10 +13,11 @@ const Interests = () => {
       <style jsx>{`
         .mainContainer {
           max-width:100%;
-          height: 100vh;
+          min-height: 100%;
+          height:100%;
           display: flex;
           flex-direction: column;
-          align-items: center;
+          align-items: stretch;
           justify-content: center;
         }        
       `}</style>
