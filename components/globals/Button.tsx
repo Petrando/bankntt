f51e8a9b9f84d4cb8ee073cbbb0e62b4baf15912
Petrando <bangkitwisata@gmail.com>
2021-08-19 17:08:57 +0800

    feat:produk dana page

--- components/globals/Button.tsx
+++ components/globals/Button.tsx
@@ -1,41 +1,53 @@
-import { any } from "prop-types";
 import { FC } from "react";
 import styles from "../../styles/components/Button.module.css";
 
 interface ButtonI {
   label: string;
+  icon?:JSX.Element;
   fullwidth?: boolean;
   onClickHandler?: (event:any)=>Promise<void> | ((event:any)=>void);
 }
 
-const Button: FC<ButtonI> = ({ label, fullwidth, onClickHandler }) => {
+const Button: FC<ButtonI> = ({ label, icon, fullwidth, onClickHandler }):JSX.Element => {
 
   return (
     <button
       type="button"
-      className={`${styles.button} ${fullwidth && styles.fullwidth}`}
+      className={`${styles.button} ${fullwidth && styles.fullwidth} ${icon && "centerRowFlex"}`}
       onClick={typeof onClickHandler!=="undefined"?onClickHandler:()=>{}}
     >
       {label}
+      {
+        icon && <>{icon}</>
+      }
     </button>
   );
 };
 
-export const MediaQueryButton: FC<ButtonI> = ({ label, fullwidth }) => {
+export const MediaQueryButton: FC<ButtonI> = ({ label, icon, fullwidth, onClickHandler }):JSX.Element => {
   return (
     <button
       type="button"
-      className={`${styles.buttonWithMediaQuery} ${fullwidth && styles.fullwidth}`}
+      className={`${styles.buttonWithMediaQuery} ${fullwidth && styles.fullwidth} ${icon && "centerRowFlex"}`}
+      onClick={typeof onClickHandler!=="undefined"?onClickHandler:()=>{}}
     >
       {label}
+      {
+        icon && <>{icon}</>
+      }
     </button>
   );
 };
 
-export const AquaButton = ({ label }) => {
+export const AquaButton = ({ label, icon, fullwidth, onClickHandler }:ButtonI):JSX.Element => {
   return (
-    <button type="button" className={`${styles.button} ${styles.aquaButton}`}>
+    <button type="button" className={`${styles.button} ${styles.aquaButton} ${icon && "centerRowFlex"}`}
+      onClick={typeof onClickHandler!=="undefined"?onClickHandler:()=>{}}
+    >
       {label}
+      {
+        icon && <>{icon}</>
+      }
     </button>
   );
 };
--- pages/produk/dana.tsx
+++ pages/produk/dana.tsx
@@ -1,10 +1,58 @@
+import React, {useRef} from "react";
+import Link from "next/link";
+import {AccountBalanceWallet, Atm, CreditCardTwoTone, AttachMoney, PhoneAndroid, PhonelinkSetup} from '@material-ui/icons/';
 import Layout from "../../components/layout";
-import UnderConstruction from "../../components/UnderConstruction";
+import Button, {AquaButton} from "../../components/globals/Button";
+import styles from "../../styles/produk/DanaMain.module.css";
 
 const Dana = () => {
+    const myRef = useRef<HTMLFieldSetElement>(null);
+    const DanaButton = ({idx, isTop, label, to, icon}:
+                        {idx:number, isTop:boolean, label:string, to:string, icon:JSX.Element}):
+                        React.ReactNode => 
+    {
+        return (
+            <Link key={idx} href={to}>
+                <a className={`${styles.anchorStyle} ${"centerRowFlex"}`}>
+                    {
+                        isTop?
+                        <Button label={label} icon={icon} />:
+                        <AquaButton label={label} icon={icon} />                                                                                
+                    }                  	
+                </a>
+          	</Link>
+        )
+    }
+
     return (
         <Layout>
-            <UnderConstruction />
+            <main className={styles.main} >
+                <div className={styles.mainOverlay} />
+                <fieldset className={`${styles.fieldsetStyle}`}>
+                    <legend>Produk Dana</legend>
+                    {
+                        [
+                         {label:"Tabungan", to:"#", icon:<AccountBalanceWallet />}, 
+                         {label:"Deposito", to:"#", icon:<CreditCardTwoTone />}, 
+                         {label:"Giro", to:"#", icon:<AttachMoney />}
+                        ].map((d, i)=>DanaButton({idx:i, isTop:true, ...d}))
+                    }
+                </fieldset>
+                <fieldset className={`${styles.fieldsetStyle}`} ref={myRef}
+                    onMouseOver={()=>{
+                        console.log(myRef.current.offsetWidth);
+                    }}
+                >
+                    <legend>Service Lainnya</legend>
+                    {
+                        [
+                         {label:"Anjungan Tunai Mandiri", to:"#", icon:<Atm />}, 
+                         {label:"Electronic Data Capture", to:"#", icon:<PhonelinkSetup />}, 
+                         {label:"Mobile Apps", to:"#", icon:<PhoneAndroid />}
+                        ].map((d, i)=>DanaButton({idx:i, isTop:false, ...d}))
+                    }
+                </fieldset>
+            </main>
         </Layout>        
     )
 }
