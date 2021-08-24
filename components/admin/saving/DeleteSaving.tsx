4821e1fdd5e908b1ae80ced68d029fa3de766a0b
Petrando <bangkitwisata@gmail.com>
2021-08-24 12:18:48 +0800

    fix:exported DeleteSaving

---
+++ components/admin/saving/DeleteSaving.tsx
@@ -0,0 +1,12 @@
+import { savingI } from "../../../types";
+//{`data:${d.photo["Content-Type"]};base64, ${d.photo["data"]}`}
+
+const DeleteSaving = ():JSX.Element => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default DeleteSaving;
