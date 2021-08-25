cd6ae1c6b1378a5e41ee52531f6c2df6128dc3f8
Petrando <bangkitwisata@gmail.com>
2021-08-25 16:58:53 +0800

    fix:dummy saving data deleted

--- components/home/Tabungan.tsx
+++ components/home/Tabungan.tsx
@@ -5,37 +5,6 @@ import LearnMore from "../globals/LearnMore";
 import styles from "../../styles/home/Tabungan.module.css";
 import galleryStyles from "../../styles/produk/dana/GalleryTabungan.module.css";
 
-const tabungan = [
-	{
-		photo:"/images/products/dana/tabungan/simpeda.jpg",
-		title:"SIMPEDA", 
-		about:"Produk Tabungan bersama BPD seluruh Indonesia dengan keunggulan Real Time Online. Dapat diakses melalui " 
-			  + "jaringan ATM bersama di seluruh Indonesia karena terhubung dalam BPDNet Online.",
-		titleColor:"#ed4545"
-	},
-	{
-		photo:"/images/products/dana/tabungan/flobamora.jpg",
-		title:"FLOBAMORA", 
-		about:"Produk tabungan yang memberikan kemudahan dan kenyamanan dalam melakukan transaksi perbankan "
-			  + " secara real time dengan berbagai fitur unggulannya dan dapat diikutsertakan dalam program berhadiah.",
-		titleColor:"#a6b7f5"
-	},
-	{
-		photo:"/images/products/dana/tabungan/tabunganku.jpg",
-		title:"TABUNGANKU", 
-		about:"Produk TabunganKu diinisiasi oleh Pemerintah Indonesia dan Bank Indonesia, untuk menyediakan "
-			  + "kemudahan pelayanan dengan produk simpanan yang mudah dan murah bagi para pelajar dan masyarakat umum.",
-		titleColor:"#f7dd9c"
-	},
-	{
-		photo:"/images/products/dana/tabungan/ziarah.jpg",
-		title:"ZIARAH", 
-		about:"Tabungan perjalanan wisata dan ziarah. Merupakan tabungan yang khusus disediakan Bank NTT untuk "
-			  + "memfasilitasi nasabah untuk bisa melakukan perjalanan wisata ataupun ziarah.",
-		titleColor:"#bbf5a9"
-	}
-]
-
 const titleColors = ["#ed4545", "#a6b7f5", "#f7dd9c", "#bbf5a9"]
 
 const Tabungan = ({savings}:{savings:savingI[]}):JSX.Element => {
